fa554c0aff401da6022068bb41b444e6ce59c8b4
Felix <felixthe8@gmail.com>
2018-04-28 15:37:21 -0700

    Removed hamburger menu. Auto-Resize on Mobile width

--- assets/scripts/header.js
+++
@@ -1,9 +0,0 @@
-function showMenu () {
-   var navLinks = document.getElementById("links");
-
-   if (navLinks.className === "nav") {
-       navLinks.classList.add("responsive")
-   } else {
-       navLinks.classList.remove("responsive")
-   }
-}
