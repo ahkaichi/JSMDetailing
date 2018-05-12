9aa32eb21af78aa38687087a7c6a7abb2c68f4fe
Felix <felixthe8@gmail.com>
2018-05-11 18:21:34 -0700

    Update registration validation logic
    Only check the passwords if all the fields are full

--- assets/scripts/registration.js
+++ assets/scripts/registration.js
@@ -6,6 +6,9 @@ function validate(e){
 		//gets all fields with required tag
 		var inputs = document.getElementsByClassName('required')
 		var submit = true;
+
+		// boolean to validate if fields are empty
+		var filled = true;
 	
 	//this for loop goes through all the fields to make sure they are not empty
 	for(var i = 0; i <inputs.length;i++)
@@ -14,16 +17,22 @@ function validate(e){
 		if(inputs[i].value.length== 0 || inputs[i].value == null )
 		{//if it is empty we should highlight the field
 			inputs[i].classList.add("jerror");
-			submit = false;
+			filled = false;
 		}
 		
 	}
-	//if password doesn't match confirmation we should aler them 
-	if(password[0].value !== confirm[0].value){
+
+	if(filled) {
+	  //if password doesn't match confirmation we should aler them 
+	  if(password[0].value !== confirm[0].value){
 		alert("Passwords do not match please try again");
 		submit = false
-	}
+	  }
 
+	} else {
+		alert("One or more fields was empty")
+		submit = false
+	}
 
 	// If there was an error, do not submit the form
 	if(!submit){
