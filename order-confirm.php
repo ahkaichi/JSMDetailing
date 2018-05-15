3794375d746723d4fb3dbe2ddf01f99a0ba94ee3
Felix <felixthe8@gmail.com>
2018-05-15 16:16:56 -0700

    prints the order information

--- order-confirm.php
+++ order-confirm.php
@@ -1,3 +1,15 @@
+#!/usr/local/php5/bin/php-cgi
+<?php
+  session_start();
+
+  $orderVehicle = $_SESSION["vehicle-order"];
+  $orderCart = $_SESSION["cart-order"];
+  $orderPrice = $_SESSION["price-order"];
+
+  $orderNumber = mt_rand();
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,20 +34,28 @@
        <div class="topnav">
         <a href="index.html">Home</a>
         <a href="portfolio.html">Portfolio</a>
-        <a href="registration.php" class="current">Registration</a>
-        <a href="login.php">Schedule Service</a>
+        <a href="registration.php">Registration</a>
+        <a href="login.php" class="current">Schedule Service</a>
       </div>
 
      </div>
   </header>
 
+  <div class="confirm-text">
+    <h2>Order Number: <?php echo $orderNumber?></h2>
+    <h3>Thank you for your order. Please print or show this receipt within a week of order submission to have your vehicle serviced.</h3>
+    <p><b>Vehicle: </b> <?php echo $orderVehicle?></p>
+    <p><b>Price: </b>$ <?php echo $orderPrice ?></p>
+    <p><b>Service(s) Purchased: </b> <?php echo nl2br($orderCart)?></p>
+  </div>
 
 
-    <div class="footer">
-      <p id="disclaimer">This website was created for a student project at CSULB and is not meant for commerical use.</p>
-      <p>Created by - Felix Huang, Andrew Kaichi, Raymond Chin, Jordan Lever. CSULB CECS 470 Spring 2018</p>
-      <p>  <?php echo "Last modified: " . date ("F d Y H:i:s.", getlastmod());?></p>
-    </div>
+
+  <div class="footer">
+    <p id="disclaimer">This website was created for a student project at CSULB and is not meant for commerical use.</p>
+    <p>Created by - Felix Huang, Andrew Kaichi, Raymond Chin, Jordan Lever. CSULB CECS 470 Spring 2018</p>
+    <p>  <?php echo "Last modified: " . date ("F d Y H:i:s.", getlastmod());?></p>
+  </div>
     
 
     <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
